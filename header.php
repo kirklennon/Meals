de988abe4615082fcc399cbf101a378e2b564b3e
Kirk Lennon <kirklennon@me.com>
2019-11-24 08:48:00 -0800

    Update header.php
    CSS edit for ingredients to add red strikethrough on hover

--- header.php
+++ header.php
@@ -5,4 +5,8 @@ td {padding:2px;
 	border: 1px solid #eee;}
 .container {max-width:35em;
 	margin:auto;}
+li a {text-decoration:none;
+	color:black}
+li a:hover {text-decoration:line-through;
+	color:red;}
 </style>
